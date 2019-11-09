2cbab31893ed4e6fdcffeabd02a125487abbc9ee
Darren Kitchen <darren@hak5.org>
2019-11-08 17:48:25 -0800

    Update payload.sh

--- payloads/library/util/Jack-Tester/payload.sh
+++ payloads/library/util/Jack-Tester/payload.sh
@@ -20,6 +20,8 @@ function run() {
   
   # Set NETMODE to DHCP_CLIENT for Shark Jack v1.1.0+
   NETMODE DHCP_CLIENT
+  # Wait for an IP address to be obtained
+  while ! ifconfig eth0 | grep "inet addr"; do sleep 1; done
   
   # Find IP address and subnet
   for i in {1..30}; do
