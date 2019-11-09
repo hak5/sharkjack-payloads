602a7f5af6f0766c32d1e90a38ca4a772362a3ca
Darren Kitchen <darren@hak5.org>
2019-11-08 17:46:36 -0800

    Added NETMODE DHCP_CLIENT for firmware v1.1.0+

--- payloads/library/util/internet-access-tester/payload.sh
+++ payloads/library/util/internet-access-tester/payload.sh
@@ -8,11 +8,16 @@
 # obtain an IP address from DHCP, and if it can access the Internet by
 # testing a specified HTTP URL.
 #
+# LED SETUP (Magenta)... Setting NETMODE to DHCP_CLIENT
 # LED Red... No IP address from DHCP yet
 # LED Yellow... Obtained IP address from DHCP, waiting on Internet access
 # LED Green... Confirmed access to Internet
 
 PUBLIC_TEST_URL="http://www.example.com"
+
+LED SETUP
+# Set NETMODE to DHCP_CLIENT for Shark Jack v1.1.0+
+NETMODE DHCP_CLIENT
 
 LED R SOLID
 while ! ifconfig eth0 | grep "inet addr"; do sleep 1; done
