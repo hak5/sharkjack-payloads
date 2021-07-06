1302d2cda7b5f39c7eb67fd0bce6a6a13ad65f2a
monsieurmarc <marcwilliams@mailbox.org>
2021-07-06 20:00:27 +0100

    Resubmitting for Payload Awards (#42)
    * Payload based on example nmap payload but with pastebin ex-filtration
    
    * Update payload.sh
    
    * refine directory name to remove spaces
    
    refine directory name to remove spaces

--- payloads/library/recon/Nmap-With-Pastebin-Exfil/payload.sh
+++ payloads/library/recon/Nmap-With-Pastebin-Exfil/payload.sh
@@ -55,7 +55,7 @@ function setup() {
 	LED SETUP
 	# Create loot directory
 	mkdir -p $LOOT_DIR &> /dev/null
-	
+
 	# Set NETMODE to DHCP_CLIENT for Shark Jack v1.1.0+
 	NETMODE DHCP_CLIENT
 	# Wait for an IP address to be obtained
